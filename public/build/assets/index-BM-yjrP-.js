dfa9aaf422c099015beb8a3372fe2311a558884c
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-06-25 10:02:14 +0800

    update build

--- public/build/assets/index-BM-yjrP-.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-D7h03rrP.js";var e=t.createContext(void 0);function i(r){const o=t.useContext(e);return r||o||"ltr"}export{i as u};
---
+++ public/build/assets/welcome-layout-Cog-x9qZ.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DQ7aRgrC.js";import{S as e}from"./scroll-area-BJCNRbU0.js";import a from"./app-bar-jQX9N-tu.js";const l=({children:r})=>s.jsx(e,{className:"h-screen max-h-screen",children:s.jsxs("div",{className:"mx-auto w-full",children:[s.jsx(a,{}),s.jsx("div",{className:"p-8",children:r})]})});export{l as W};
